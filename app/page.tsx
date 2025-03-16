6cf1c9c500217c031a6991383cbdab117ce1f295
shadowcracks <yst187business@gmail.com>
2025-03-15 22:19:17 -0400

    modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import InventoryPag from "./components/InventoryPag";
 export default function InventoryPage() {
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-xl font-bold mb-4">Inventory Dashboard</h1>
       <InventoryPag/>
     </div>
   );
